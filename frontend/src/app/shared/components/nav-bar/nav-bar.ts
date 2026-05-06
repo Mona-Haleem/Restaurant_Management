4d6e29950589c77e9311610418a564215c5ebcf9
mona <mona.halleem@gmail.com>
2026-05-06 18:02:08 +0300

    add the top navigation bar

---
+++ frontend/src/app/shared/components/nav-bar/nav-bar.ts
@@ -0,0 +1,24 @@
+import { Component, inject } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { SearchBar } from '../../../features/customer/components/search-bar/search-bar';
+import { CartService } from '../../../core/services/cart/cart.service';
+import { AsyncPipe } from '@angular/common';
+import { firstValueFrom, map } from 'rxjs';
+//temporary has to be updated and tested.
+
+@Component({
+  selector: 'app-nav-bar',
+  imports: [MatIconModule, SearchBar, AsyncPipe],
+  templateUrl: './nav-bar.html',
+  styleUrl: './nav-bar.scss',
+})
+export class NavBar {
+  private cartService = inject(CartService)
+
+  get count() {
+    return this.cartService.count.pipe(map(c => c > 9 ? '9+' : c == 0 ? '' : c));
+  }
+
+
+
+}
