867de59a1207a9390c3d6b9fcc934e562eb3ab8b
mona <mona.halleem@gmail.com>
2026-09-11 18:20:43 +0300

    chore: setup main style variables && classes

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
 //import { RouterOutlet } from '@angular/router';
 //import { NavBar } from './shared/components/nav-bar/nav-bar';
-
+import { StyleTest } from './temp/style-test/style-test';
 @Component({
   selector: 'app-root',
-  imports: [], //RouterOutlet , NavBar],
+  imports: [StyleTest], //RouterOutlet , NavBar],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
