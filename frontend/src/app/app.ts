ee4bb8ca6514b1a070a4fe748a52628dc4591507
mona <mona.halleem@gmail.com>
2026-09-12 15:48:59 +0300

    feat: set up language servcie

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,12 +2,15 @@ import { Component, signal } from '@angular/core';
 //import { RouterOutlet } from '@angular/router';
 //import { NavBar } from './shared/components/nav-bar/nav-bar';
 //import { StyleTest } from './temp/style-test/style-test';
+//import { LanguageService } from './core/services/language/language.service';
+
 @Component({
   selector: 'app-root',
-  imports: [], //StyleTest ,RouterOutlet , NavBar],
+  imports: [], //StyleTest , RouterOutlet , NavBar],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('frontend');
+  //protected readonly languageService = inject(LanguageService);
 }
